4408803b9334d08b3fbf27f806cc39fe0a37ea56
Carson Shively <carsonshively24@gmail.com>
2026-01-13 17:55:17 -0700

    move older project from hf to git

--- modeling/eval.py
+++ modeling/eval.py
@@ -14,7 +14,7 @@ from huggingface_hub import hf_hub_download
 
 train_data = hf_hub_download(
         repo_id="Carson-Shively/uber-fares",
-        filename="data/gold/gold_uf.parquet",
+        filename="data/gold/gold.parquet",
         repo_type="dataset",
         revision="main",
     )
